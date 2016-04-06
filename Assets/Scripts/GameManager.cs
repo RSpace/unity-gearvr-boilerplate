f85da7acd7abc56c832ed4a6f27617117177003e
Casper Fabricius <me@casperfabricius.com>
2016-04-06 23:07:32 +0200

    Added primitive camera movement using the VRInput util class.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,22 @@ using VRStandardAssets.Utils;
 public class GameManager : MonoBehaviour {
 
   private VRInteractiveItem[] m_InteractiveItems;
+  private VRInput m_VRInput;
+
+  [SerializeField] Camera m_MainCamera;
+  [SerializeField] float m_MovementSpeed = 3f;
 
 	// Use this for initialization
 	void Start () {
-    // Find objects that can be interacted with via gaze
+    SetupGaze();
+    SetupMovement();
+	}
+
+  void SetupGaze() {
+    // Find items that can be interacted with via gaze
     m_InteractiveItems = GameObject.FindObjectsOfType<VRInteractiveItem>();
 
-    // Subscribe to appropriate events on each object
+    // Subscribe to appropriate events on each interactable item
     for(int i = 0; i < m_InteractiveItems.Length; i++) {
       var item = m_InteractiveItems[i];
       item.OnOver += delegate() { this.OnItemOver(item); };
@@ -19,7 +28,15 @@ public class GameManager : MonoBehaviour {
       // Also subscribeable: OnDoubleClick, OnClick, OnDown, OnUp
 
     }
-	}
+  }
+
+  void SetupMovement() {
+    // Find input manager
+    m_VRInput = GameObject.FindObjectOfType<VRInput>();
+
+    // This action is invoked on every single frame
+    m_VRInput.OnSwipe += this.OnSwipe;
+  }
 
   void OnItemOver(VRInteractiveItem item) {
     // Scale item down to half it's normal size
@@ -29,5 +46,25 @@ public class GameManager : MonoBehaviour {
   void OnItemOut(VRInteractiveItem item) {
     // Scale item back up to full size
     item.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+  }
+
+  void OnSwipe(VRInput.SwipeDirection direction) {
+    // Only do something when we have a swipe
+    if(direction != VRInput.SwipeDirection.NONE) {
+      switch(direction) {
+        case VRInput.SwipeDirection.LEFT:
+          m_MainCamera.transform.Translate(m_MovementSpeed * -1, 0, 0);
+          break;
+        case VRInput.SwipeDirection.RIGHT:
+          m_MainCamera.transform.Translate(m_MovementSpeed, 0, 0);
+          break;
+      case VRInput.SwipeDirection.UP:
+        m_MainCamera.transform.Translate(0, 0, m_MovementSpeed);
+        break;
+      case VRInput.SwipeDirection.DOWN:
+        m_MainCamera.transform.Translate(0, 0, m_MovementSpeed * -1);
+        break;
+      }
+    }
   }
 }
